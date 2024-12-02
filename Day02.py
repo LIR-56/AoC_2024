d7e34a85142563432f9f4012b872345584aec179
LIR-56 <lir-567@rambler.ru>
2024-12-02 11:00:22 +0100

    Day 2 style

--- Day02.py
+++ Day02.py
@@ -9,15 +9,16 @@ def count_safe_reports(levels_reports):
             safe_report_counter += 1
     return safe_report_counter
 
+
 def count_safe_reports_with_tolerance(levels_reports):
     safe_report_counter = 0
     for report in levels_reports:
         report_check = report_is_safe(report)
         if report_check == 42:
             safe_report_counter += 1
-        elif (report_is_safe(rbi(report, report_check - 1)) == 42 or #10 6 4 3 2 1
-                report_is_safe(rbi(report, report_check)) == 42 or
-                (report_check > 1 and report_is_safe(rbi(report, report_check - 2)) == 42)):  #4 6 4 3 2 1
+        elif (report_is_safe(rbi(report, report_check - 1)) == 42 or  # 10 6 4 3 2 1
+              report_is_safe(rbi(report, report_check)) == 42 or
+              (report_check > 1 and report_is_safe(rbi(report, report_check - 2)) == 42)):  # 4 6 4 3 2 1
             safe_report_counter += 1
     return safe_report_counter
 
@@ -30,7 +31,7 @@ def rbi(some_list, index):  # remove by index
 
 def report_is_safe(levels):
     i = 1
-    increase = levels[0] < levels[1] #all reports have at least 4 levels
+    increase = levels[0] < levels[1]  # all reports have at least 4 levels
     while i < len(levels):
         if (abs(levels[i - 1] - levels[i]) > 3 or abs(levels[i - 1] - levels[i]) < 1) \
                 or (increase and levels[i - 1] >= levels[i]) \
